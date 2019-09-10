690dfc088d975bf0028e83162a54b0ba7b4a3b6d
admin <admin@itheima.com>
2019-09-10 10:47:12 +0800

    把本地工程添加到git仓库

---
+++ src/com/itheima/test/FirstClass.java
@@ -0,0 +1,21 @@
+package com.itheima.test;
+
+/**
+ * FirstClass
+ *
+ * @author Hey
+ * @create 2019-09-10 10:40
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+        System.out.println("Hello Git Project!");
+    }
+}
